3b4df433e1cea4e883d5895b0ceb75fcf06a8858
Mario_95 <mrojasc95.mr@gmail.com>
2020-02-09 19:18:04 -0500

    images downloades/ hierarchy strcutured
    characters images doenloades file hierarchy structures(react app still not working properly)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import characterCard from "./componets/charcterCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import characters from "./characters.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.characters to the characters json array
+  state = {
+    characters
+  };
+
+  removeFriend = id => {
+    // Filter this.state.characters for characters with an id not equal to the id being removed
+    const characters = this.state.characters.filter(friend => friend.id !== id);
+    // Set this.state.characters equal to the new characters array
+    this.setState({ characters });
+  };
+
+  // Map over this.state.characters and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Super Heros and Villans</Title>
+        {this.state.characters.map(character => (
+          <characterCard
+            removecharacter={this.removecharacter}
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
